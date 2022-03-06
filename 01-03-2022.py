09651768f02ec154d4f8b6ec4867074007671f91
Portablefire22 <39838509+Portablefire22@users.noreply.github.com>
2022-03-06 01:30:26 +0000

    Update 01-03-2022.py
    Added colon for formatting and making inputs more readable by the user.

--- 01-03-2022.py
+++ 01-03-2022.py
@@ -1,6 +1,6 @@
 def main():
-  sr = str(input("Give me an input"))
-  intg = int(input("Give me a number"))
+  sr = str(input("Give me an input: "))
+  intg = int(input("Give me a number: "))
   print(sr*intg)
   return 0
 
